f7ecc83add7adf180c70882e777e50002aae8fe0
MarLion <925223337@qq.com>
2018-12-06 21:07:50 +0800

    Update base.js

--- admin_system_new/src/axios/base.js
+++ admin_system_new/src/axios/base.js
@@ -1,3 +1,2 @@
 //export const baseUrl = ''; //生产环境
-export const baseUrl = 'http://192.168.1.30:3001/business-hongniang-service/'; //测试环境
-//export const baseUrl = 'http://192.168.1.48:6001/'; //刘爽本地
+export const baseUrl = ''; //测试环境
