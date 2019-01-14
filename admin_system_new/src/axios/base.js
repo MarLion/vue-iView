16ccc105b8e7fe1d8ce790e807b4da5604b3b9b9
MarLion <925223337@qq.com>
2019-01-14 16:03:00 +0800

    Update base.js

--- admin_system_new/src/axios/base.js
+++ admin_system_new/src/axios/base.js
@@ -1,9 +1,9 @@
 //export const baseUrl = {
-// serviceOne:'http://112.74.187.103/business-hongniang-service/',
-// serviceTwo:'http://112.74.187.103/system-im-service/'
+// serviceOne:'',
+// serviceTwo:''
 // }; //生产环境
 export const baseUrl = {
-  serviceOne:'http://192.168.1.197:3001/business-hongniang-service/',
-  // serviceOne:'http://192.168.1.30:3001/business-hongniang-service/',//30地址
-  serviceTwo:'http://192.168.1.197:3001/system-im-service/'
+  serviceOne:'',
+  // serviceOne:'',//30地址
+  serviceTwo:''
 }; //测试环境
