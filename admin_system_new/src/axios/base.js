bc7c999d8d35afc847a4c4e5578a6de8a8917623
sheyu <925223337@qq.com>
2019-04-27 09:26:03 +0800

    删除部分数据

--- admin_system_new/src/axios/base.js
+++ admin_system_new/src/axios/base.js
@@ -1,7 +1,4 @@
-//export const baseUrl = {
-// serviceOne:'http://112.74.187.103/business-hongniang-service/',
-// serviceTwo:'http://112.74.187.103/system-im-service/'
-// }; //生产环境
+
 export const baseUrl = {
   serviceOne:'http://192.168.1.197:3001/business-hongniang-service/',//测试服
   serviceTwo:'http://192.168.1.197:3001/system-im-service/',
@@ -9,10 +6,4 @@ export const baseUrl = {
   serviceFour:'http://192.168.1.197:3001/system-employee-service/',
   serviceFive:'http://192.168.1.197:3001/business-central-service/',
   serviceSix:'http://192.168.1.198:6004/'
-  // serviceOne:'http://112.74.187.103/business-hongniang-service/',//正式
-  // serviceTwo:'http://112.74.187.103/system-im-service/',
-  // serviceThree:'http://112.74.187.103/business-user-service/',
-  // serviceFour:'http://112.74.187.103/system-employee-service/',
-  // serviceFive:'http://112.74.187.103/business-central-service/',
-  // serviceSix:'http://yys.zhongwei-info.com/business-health-service/'
 };
